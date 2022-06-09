be4f84523b5d1f43de7850655af469bc58754787
oktovan Agung Shaiendra <72904723+OktovanAgung@users.noreply.github.com>
2022-06-09 20:53:48 +0700

    update artikelphp.png and Artikel.php

--- lab11_ci/ci4/app/Controllers/Artikel.php
+++ lab11_ci/ci4/app/Controllers/Artikel.php
@@ -7,4 +7,10 @@ use App\Models\ArtikelModel;
 class Artikel extends BaseController
 {
     public function index()
+    {
+        $title = 'Daftar Artikel';
+        $model = new ArtikelModel();
+        $artikel = $model->findALL();
+        return view('artikel/index', compact('artikel','title'));
+    }
 }
